71ffb212362434fd02c92584f925c60c1cafac82
Ari <arian.parvin@parvin.de>
2019-04-22 13:56:13 +0200

    search-page wip

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  githubId: {
+    type: String
+  },
+  facebookId: {
+    type: String
+  },
+  displayName: {
+    type: String
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user'
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
--- routes/search.js
+++ routes/search.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-/* Get landing-page */
-router.get('/', (req, res, next) => {
-  res.render('index');
-});
 //Get Page 2//
 router.get('/search', (req, res, next) => {
   res.render('search')
