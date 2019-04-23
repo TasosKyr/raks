17781a1543ba3572396b74774e3a1d1d51bf3fbf
Ari <arian.parvin@parvin.de>
2019-04-23 19:28:48 +0200

    okay again

--- routes/profile.js
+++ routes/profile.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose')
-const router = express.Router();
+const profileRouter = express.Router();
 const Identicon = require("identicon")
 const jsSHA = require("jssha");
+const MovieCollection = require('../models/MovieCollection');
+
 
 // Identicon Setup, unfinished
 var shaObj = new jsSHA("SHA-512", "TEXT");
 shaObj.update(`${user.username}`);
 var hash = shaObj.getHash("HEX");
 var data = new Identicon(hash, { format: 'svg' }).toString();
+
+// cart routing
+profileRouter.post('/search', (req, res, next) => {
+  res.redirect('/search');
+  MovieCollection.create({
+    name: 'Rak 1',
+    _owner: user._id
+  });
+})
