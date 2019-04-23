1a9d97d548b33341e673c37bb847c5c16aa9b6e5
Hamza Al Ahmar <hamzaahmar21@gmail.com>
2019-04-23 18:28:03 +0200

    model User

--- models/User.js
+++ models/User.js
@@ -1,30 +1,22 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  githubId: {
-    type: String
-  },
-  facebookId: {
-    type: String
-  },
-  displayName: {
-    type: String
-  },
-  role: {
-    type: String,
-    enum: ['user', 'admin'],
-    default: 'user'
-  }
-});
+const userSchema = new Schema(
+    {
+        username: String,
+        googleId: String,
+        password: String,
+        movieCollection: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'MovieCollection'
+            }
+        ]
+    },
+    {
+        timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
+    }
+);
 
 const User = mongoose.model('User', userSchema);
 
