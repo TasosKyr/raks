dee0808175f8ae6467cbc8dca15db2ef6a8fa8c8
Ari <arian.parvin@parvin.de>
2019-04-23 15:53:38 +0200

    Profile page stable / identicon wip

--- models/user.js
+++ models/user.js
@@ -1,15 +1,30 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema(
-    {
-        username: String,
-        password: String
-    },
-    {
-        timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
-    }
-);
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  githubId: {
+    type: String
+  },
+  facebookId: {
+    type: String
+  },
+  displayName: {
+    type: String
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user'
+  }
+});
 
 const User = mongoose.model('User', userSchema);
 
--- routes/profile.js
+++ routes/profile.js
@@ -1,4 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const router = express.Router();
+const Identicon = require("identicon")
+const jsSHA = require("jssha");
 
-
+// Identicon Setup, unfinished
+var shaObj = new jsSHA("SHA-512", "TEXT");
+shaObj.update(`${user.username}`);
+var hash = shaObj.getHash("HEX");
+var data = new Identicon(hash, { format: 'svg' }).toString();
