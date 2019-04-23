e56e10d2c04489f9ed1439f8e1e6321a6769074c
Tasos Kyr <tasoskyr1@gmail.com>
2019-04-23 11:33:47 +0200

    Get genres names

--- routes/index.js
+++ routes/index.js
@@ -23,11 +23,105 @@ router.post('/search', (req, res, next) => {
             const { data } = response;
             console.log(data.results);
             let results = data.results;
+            data.results.forEach(el => {
+                const genre_ids = el.genre_ids;
+                const genre_names = getIdName(genre_ids);
+                el.genre_names = genre_names;
+            });
             res.render('search', { results });
         })
         .catch(err => {
             console.error(err);
         });
 });
+
+// Get genres names from the API
+const genreIds = [
+    {
+        id: 28,
+        name: 'Action'
+    },
+    {
+        id: 12,
+        name: 'Adventure'
+    },
+    {
+        id: 16,
+        name: 'Animation'
+    },
+    {
+        id: 35,
+        name: 'Comedy'
+    },
+    {
+        id: 80,
+        name: 'Crime'
+    },
+    {
+        id: 99,
+        name: 'Documentary'
+    },
+    {
+        id: 18,
+        name: 'Drama'
+    },
+    {
+        id: 10751,
+        name: 'Family'
+    },
+    {
+        id: 14,
+        name: 'Fantasy'
+    },
+    {
+        id: 36,
+        name: 'History'
+    },
+    {
+        id: 27,
+        name: 'Horror'
+    },
+    {
+        id: 10402,
+        name: 'Music'
+    },
+    {
+        id: 9648,
+        name: 'Mystery'
+    },
+    {
+        id: 10749,
+        name: 'Romance'
+    },
+    {
+        id: 878,
+        name: 'Science Fiction'
+    },
+    {
+        id: 10770,
+        name: 'TV Movie'
+    },
+    {
+        id: 53,
+        name: 'Thriller'
+    },
+    {
+        id: 10752,
+        name: 'War'
+    },
+    {
+        id: 37,
+        name: 'Western'
+    }
+];
+
+const getIdName = arrIds => {
+    // genre_ids: [ 10770, 14, 16, 27, 28, 53, 878 ],
+    let genreArray = arrIds.map(id => {
+        let foundObj = genreIds.find(el => el.id === id);
+        return foundObj.name;
+    });
+    return genreArray;
+};
 
 module.exports = router;
