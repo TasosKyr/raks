e0bc1e6e08b79705d45870599bd9fd6adbce7eaf
Ari <arian.parvin@parvin.de>
2019-04-24 15:13:33 +0200

    profile tinkering

--- routes/profile.js
+++ routes/profile.js
@@ -3,15 +3,27 @@ const profileRouter = express.Router();
 const Identicon = require("identicon")
 const jsSHA = require("jssha");
 const MovieCollection = require('../models/MovieCollection');
-
+const User = require('../models/User');
 
 // Identicon Setup, unfinished
-var shaObj = new jsSHA("SHA-512", "TEXT");
-shaObj.update(`${user.username}`);
-var hash = shaObj.getHash("HEX");
-var data = new Identicon(hash, { format: 'svg' }).toString();
+// var template = document.getElementById('idcon').innerHTML;
+// var renderId = Handlebars.compile(template);
+// var shaObj = new jsSHA("SHA-512", "TEXT");
+// shaObj.update(`${_id}`);
+// var hash = shaObj.getHash("HEX");
+// renderId = new Identicon(hash, { format: 'svg' }).toString();
 
-// cart routing
+
+//User rak rendering
+MovieCollection.find({ _owner: _id }
+  .then(collections => {
+    res.render('profile', { collections })
+  }))
+  .catch(err => {
+    console.error("failed to render user collection", err)
+  })
+
+// cart creation
 profileRouter.post('/search', (req, res, next) => {
   res.redirect('/search');
   MovieCollection.create({
