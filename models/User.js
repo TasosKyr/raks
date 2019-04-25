ed2427664cc3c035c5b63f8d6392ee2164cd2f2f
Hamza Al Ahmar <hamzaahmar21@gmail.com>
2019-04-25 11:38:00 +0200

    added User model

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+    {
+        /* _id: {
+            type: String,
+            required: true
+        }, */
+        username: {
+            type: String,
+            required: true
+        },
+        googleId: String,
+        password: String,
+        movieCollection: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'MovieCollection'
+            }
+        ]
+    },
+    {
+        timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
+    }
+);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
