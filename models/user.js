85452b2844a5220d9155089567ce67d1d840a9b4
Hamza Al Ahmar <hamzaahmar21@gmail.com>
2019-04-25 10:41:28 +0200

    stying Signup page

--- models/user.js
+++ models/user.js
@@ -1,15 +1,28 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema(
-    {
-        username: String,
-        password: String
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true
     },
-    {
-        timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
+    password: {
+        type: String,
+        required: true
+    },
+
+    googleId: {
+        type: String
+    },
+    displayName: {
+        type: String
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
     }
-);
+});
 
 const User = mongoose.model('User', userSchema);
 
